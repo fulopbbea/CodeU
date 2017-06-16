a54f80385ef82df7589ceba512d6a2a58ee1fc73
fulopbbea <fulopbbea93@gmail.com>
2017-06-16 16:12:01 +0200

    Requested changes
    -deleted redundant condition checks
    -added 2 new test cases
    -refactored code

--- src/assignment3/Assignment3.java
+++ src/assignment3/Assignment3.java
@@ -5,7 +5,7 @@ import java.util.Set;
 
 public class Assignment3 {
 
-    private static void recursiveSearch(char[][] grid, boolean visited[][],
+    private static void recursiveSearchForWords(char[][] grid, boolean visited[][],
             int width, int height, int startI, int startJ,
             Dictionary dict, String prefix, Set<String> foundWords) {
         
@@ -25,7 +25,7 @@ public class Assignment3 {
                 if (visited[i][j] == false) {
                     String prefixAug = prefix.concat(String.valueOf(grid[i][j]));
                     if (dict.isPrefix(prefixAug)) {
-                        Assignment3.recursiveSearch(grid, visited, width, height,
+                        recursiveSearchForWords(grid, visited, width, height,
                                 i, j, dict, prefixAug, foundWords);
                     }
                 }
@@ -34,34 +34,22 @@ public class Assignment3 {
         visited[startI][startJ] = false;
     }
     
-    public static Set<String> findWords(char[][] grid,
-            int width, int height, Dictionary dict) {
+    public static Set<String> findWords
+        (char[][] grid, int width, int height, Dictionary dict) {
         
         Set<String> foundWords = new HashSet<>();
         
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                if (foundWords.size() < dict.size()) {
-                    if (dict.isPrefix(String.valueOf(grid[i][j]))) {
-                        boolean visited[][] = new boolean[height][width];
-                        for (int m = 0; m < height; m++) {
-                            for (int n = 0; n < width; n++) {
-                                visited[m][n] = false;
-                            }
-                        }
-                        Assignment3.recursiveSearch(grid, visited,
-                                width, height, i, j,
-                                dict, String.valueOf(grid[i][j]), foundWords);
-                    }
-                } else {
-                    break;
+                if (dict.isPrefix(String.valueOf(grid[i][j]))) {
+                    boolean visited[][] = new boolean[height][width];
+                    recursiveSearchForWords(grid, visited,
+                            width, height, i, j,
+                            dict, String.valueOf(grid[i][j]), foundWords);
                 }
             }
         }
         return foundWords;
-    }
-    
-    public static void main(String[] args) {
     }
     
 }
--- test/assignment3/Assignment3Test.java
+++ test/assignment3/Assignment3Test.java
@@ -5,25 +5,13 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class Assignment3Test {
     
     private static final int HEIGHT = 2;
     private static final int WIDTH = 3;
-    private static final char[][] GRID = new char[HEIGHT][WIDTH];
-    
-    @BeforeClass
-    public static void setUpClass() {
-        Assignment3Test.GRID[0][0] = 'a';
-        Assignment3Test.GRID[0][1] = 'a';
-        Assignment3Test.GRID[0][2] = 'r';
-        
-        Assignment3Test.GRID[1][0] = 't';
-        Assignment3Test.GRID[1][1] = 'c';
-        Assignment3Test.GRID[1][2] = 'd';
-    }
+    private static final char[][] GRID = {{'a', 'a', 'r'}, {'t', 'c', 'd'}};
 
     @Test
     public void testFindWordsGeneric() {
@@ -48,7 +36,7 @@ public class Assignment3Test {
         System.out.println("testFindWordsVisitedOrder");
         Dictionary dict = new Dictionary(Arrays.asList("car", "card", "cart", "cat", "aca"));
         Set<String> result = Assignment3.findWords(GRID, WIDTH, HEIGHT, dict);
-        Assert.assertEquals("Two Hits", result,
+        Assert.assertEquals("Visited Order", result,
                 new HashSet<>(Arrays.asList("car", "card", "cat", "aca")));
     }
     
@@ -66,8 +54,7 @@ public class Assignment3Test {
         System.out.println("testFindWordsEmptyDict");
         Dictionary dict = new Dictionary(new ArrayList<>());
         Set<String> result = Assignment3.findWords(GRID, WIDTH, HEIGHT, dict);
-        Assert.assertEquals("Empty Dict", result,
-                new HashSet<>());
+        Assert.assertEquals("Empty Dict", result, new HashSet<>());
     }
     
     @Test
@@ -77,5 +64,21 @@ public class Assignment3Test {
         Set<String> result = Assignment3.findWords(GRID, WIDTH, HEIGHT, dict);
         Assert.assertEquals("Empty String", result,
                new HashSet<>(Arrays.asList("car", "card", "cat")));
+    }
+    
+    @Test
+    public void testFindWordsNoWOrdsFound() {
+        System.out.println("testFindWordsNoWordsFound");
+        Dictionary dict = new Dictionary(Arrays.asList("adat", "radt"));
+        Set<String> result = Assignment3.findWords(GRID, WIDTH, HEIGHT, dict);
+        Assert.assertEquals("No Words Found", result, new HashSet<>());
+    }
+    
+    @Test
+    public void testFindWordsEmptyGrid() {
+        System.out.println("testFindWordsEmptyGrid");
+        Dictionary dict = new Dictionary(Arrays.asList("car", "card", "cart", "cat", "dart"));
+        Set<String> result = Assignment3.findWords(null, 0, 0, dict);
+        Assert.assertEquals("Empty Grid", result, new HashSet<>());
     }
 }
